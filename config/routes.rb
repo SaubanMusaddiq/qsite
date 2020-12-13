6884a82e56c242c4076e6107e7680de315b7598b
sauban <musaddiq.sauban@gmail.com>
2020-12-12 18:50:38 -0700

    sf: config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  get 'chapters', to: "chapters#index"
+  # get 'chapters/:id', to: "chapters#show" , as:"chapter"
+  # get 'chapters/:id/verses', to: "chapters#show"
+  # get 'chapters/:id/verses/:v_id1', to: "verses#show", as:"chapter_verse"
+  # get 'chapters/:id/verses/:v_id1/:v_id2', to: "verses#show", as:"chapter_verses"
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  get    '/logout',  to: 'sessions#destroy'
+  root 'welcome#index'
+  get    '/admin',   to: 'users#admin'
+  get    '/admin/analytics',   to: 'users#analytics'
+  get    '/admin/configs',   to: 'users#configs'
+  get    '/admin/files',   to: 'users#files'
+  post   '/admin/file_save/:id', to: 'users#file_save'
+  get    '/admin/download_file_kn/:id',   to: 'users#download_file_kn'
+  get    '/admin/download_file_ar/:id',   to: 'users#download_file_ar'
+
+  get '/:id', to: "chapters#show" , as:"chapter"
+  get '/:id/:v_id1', to: "verses#show", as:"chapter_verse"
+
 end
