29f899b0738fb303ecdbea1517a0e3063f8e46d2
sauban <musaddiq.sauban@gmail.com>
2021-11-28 18:12:33 -0700

    Chapter nav in all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :set_variables
+
+def set_variables
+  @all_chapters = Chapter.all
+  @chapter = Chapter.find(1)
+  @show_verses_in_nav = false
+end
 
   def authenticate_user
     if !logged_in?
       redirect_to login_path
     end
-  end  
-	
+  end
+
 end
--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,10 +1,9 @@
 class ChaptersController < ApplicationController
   def index
-    @all_chapters = Chapter.all
   end
   def show
-  	@all_chapters = Chapter.all
   	@chapter = Chapter.find(params[:id])
   	@all_verses = @chapter.verses
-  end	
+    @show_verses_in_nav = true
+  end
 end
